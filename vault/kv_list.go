044218c9daab4b99050a30ae99df252e4a296307
Sean Lingren <srlingren@gmail.com>
2018-05-08 18:10:20 -0500

    factor out list parse

--- vault/kv_list.go
+++ vault/kv_list.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 
 	"github.com/pkg/errors"
+
+	vapi "github.com/hashicorp/vault/api"
 )
 
 // KVListInput is the required input for KVList
@@ -22,6 +24,41 @@ func NewKVListInput(p string) *KVListInput {
 		Recurse:        false,
 		TrimPathPrefix: true,
 	}
+}
+
+// getKVListFromSecret takes a vault api *Secret returned by List()
+// and parses the data into a list of keys
+func getKVListFromSecret(s *vapi.Secret) ([]string, error) {
+	var err error
+	var output []string
+
+	if s == nil || s.Data == nil {
+		return nil, errors.Wrap(err, "Secret was nil")
+	}
+
+	keys, ok := s.Data["keys"]
+	if !ok || keys == nil {
+		return nil, errors.Wrap(err, "No Data[\"keys\"] in secret")
+	}
+
+	list, ok := keys.([]interface{})
+	if !ok {
+		return nil, errors.Wrap(err, "Failed to convert keys to interface")
+	}
+
+	for _, v := range list {
+		key, ok := v.(string)
+		if !ok {
+			return nil, errors.Wrapf(err, "Failed to assert %s as a string", key)
+		}
+		output = append(output, key)
+	}
+
+	return output, err
+}
+
+func listPath(p string) ([]string, error) {
+	return nil, nil
 }
 
 // KVList takes a path and returns a slice of all values at that path
@@ -48,26 +85,16 @@ func (c *Client) KVList(i *KVListInput) ([]string, error) {
 	}
 
 	secret, err := c.client.Logical().List(listPath)
-	if err != nil || secret == nil || secret.Data == nil {
+	if err != nil {
 		return nil, errors.Wrapf(err, "Failed to list path %s", listPath)
 	}
 
-	keys, ok := secret.Data["keys"]
-	if !ok || keys == nil {
-		return nil, errors.Wrapf(err, "No Data[\"keys\"] at path %s", i.Path)
+	keys, err := getKVListFromSecret(secret)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Failed to parse secret into keys", i.Path)
 	}
 
-	list, ok := keys.([]interface{})
-	if !ok {
-		return nil, errors.Wrapf(err, "Failed to convert %s keys to interface", i.Path)
-	}
-
-	for _, v := range list {
-		key, ok := v.(string)
-		if !ok {
-			return nil, errors.Wrapf(err, "Failed to assert %s in %s as a string", key, i.Path)
-		}
-
+	for _, key := range keys {
 		if c.PathIsFolder(key) && i.Recurse {
 			subKeys, _ := c.KVList(&KVListInput{
 				Path:           c.PathJoin(i.Path, key),
