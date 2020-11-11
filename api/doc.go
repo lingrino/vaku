3e12e2343d53cf69805948b79cdc7efd6146a6ad
Sean Lingren <sean@lingrino.com>
2020-11-11 12:35:04 -0800

    package comments and timeout

---
+++ api/doc.go
@@ -0,0 +1,7 @@
+/*
+Package vaku provides an API for interacting with the Vault Key/Value secrets engine. Vaku works
+indentically on V1 and V2 K/V mount versions except in cases where the command is only supported on
+the V2 engine. Vaku supports many useful commands like copy, move, and search on Vault paths and
+folders alike.
+*/
+package vaku
